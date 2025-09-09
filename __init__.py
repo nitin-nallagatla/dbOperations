8af5658547bd7ef7b83c9d71540b9790f0f477ae
nitin-nallagatla <nitinben9@gmail.com>
2025-09-09 13:29:59 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,4 +2,4 @@ import logging
 
 logging.basicConfig(filename='mySqlClass.log', filemode='w',
                     format='[{%(asctime)s} %(filename)s: %(lineno)d]\t%(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+                    level=logging.INFO) 
